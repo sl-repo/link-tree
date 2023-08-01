4cdac960fa2e87b9abe721dcb305b6c205b43716
MSGlobalShala <manish@globalshala.com>
2023-08-01 19:01:46 +0530

    fixed an issue

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 const port = 3000;
@@ -11,31 +11,32 @@ app.use(bodyParser.json());
 http
   .createServer((req, res) => {
     const userAgent = req.headers["user-agent"];
-   // const file = "./localhost-sc.pdf";
-   if (userAgent.includes('Instagram')) {
-    fs.readFile(file, (err, data) => {
-      if (err) {
-        res.writeHead(500, { "Content-Type": "text/plain" });
-        res.end("Error reading the file.");
-      } else {
-        const smallByteContent =
-          "data:application/octet-stream;base64,AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
-        res.setHeader("Content-Type", "application/octet-stream");
-        res.setHeader(
-          "Content-Disposition",
-          "attachment; filename=small_file.txt"
-        );
-        res.end(Buffer.from(smallByteContent, "base64"));
-        res.end(data);
-      }
-    });
+    // const file = "./localhost-sc.pdf";
+    if (userAgent.includes("Instagram")) {
+      fs.readFile(file, (err, data) => {
+        if (err) {
+          res.writeHead(500, { "Content-Type": "text/plain" });
+          res.end("Error reading the file.");
+        } else {
+          const smallByteContent =
+            "data:application/octet-stream;base64,AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA";
+          res.setHeader("Content-Type", "application/octet-stream");
+          res.setHeader(
+            "Content-Disposition",
+            "attachment; filename=small_file.txt"
+          );
+          res.end(Buffer.from(smallByteContent, "base64"));
+          res.end(data);
+        }
+      });
     } else {
-        res.writeHead(302, { 'Location': "https://experience.4excelerate.org/external/knowMore/Competition/M9MH7CS" });
-        res.end();
+      res.writeHead(302, {
+        Location:
+          "https://experience.4excelerate.org/external/knowMore/Competition/M9MH7CS",
+      });
+      res.end();
     }
   })
-
-
-  app.listen(port, () => {
+  .listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
   });
